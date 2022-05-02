1a20fa40011f91730f8063d060f03627cc759f5a
Kevin Golan <golan.kevin@gmail.com>
2022-05-02 12:05:54 +0200

    Created Data Analysis function that reports basic info on the content of each recorded sample in this task

--- python_src/Zurich_Path_Detection_Submission_Template.py
+++ python_src/Zurich_Path_Detection_Submission_Template.py
@@ -5,14 +5,18 @@
 # examplestudent2@ethz.ch, 12-345-679
 # examplestudent3@ethz.ch, 12-345-670
 
+from cProfile import label
 import sys
+import os
 from Lilygo.Recording import Recording
 from Lilygo.Dataset import Dataset
+import json
+
 sys.path.append("..")
 
-filename = sys.argv[1]  # e.g. 'data/someDataTrace.json'
+# filename = sys.argv[1]  # e.g. 'data/someDataTrace.json'
 # IMPORTANT: To allow grading, the two arguments no_labels and mute must be set True in the constructor when loading the data
-trace = Recording(filename, no_labels=False, mute=False)
+# trace = Recording(filename, no_labels=False, mute=False)
 boardLocation = 0  # <- here goes your detected board location
 pathIdx = 0  # <- here goes your detected path index
 stepCount = 0  # <- here goes your detected stepcount
@@ -26,8 +30,43 @@ activities = (
 # Make sure, you do not use the gps data and are tolerant for missing data (see task set). Your program must not crash when single smartphone data traces are missing.
 #
 
-def 
 
+def activities_detector(recordings, labelled_activities):
+    print(recordings.keys())
+    print("Activity types: c = cycling; r = running; w = walking; s = standing")
+
+
+def boardLocation_detector():
+    pass
+
+
+def data_analysis(recordings_folder_path):
+    for i in range(1):
+        sample = os.listdir(recordings_folder_path)[i]
+        with open(os.path.join(recordings_folder_path, sample)) as sample_json:
+            labelled_sample = json.load(sample_json)
+        print("Data Labels:", labelled_sample["labels"])
+        print(
+            "Data Fields:\n", labelled_sample["data"][0].keys()
+        )  # Zero'th element because "data" field is a list containing a dictionary of values {[]}
+
+        field_lengths = dict.fromkeys(
+            labelled_sample["data"][0].keys(),
+        )
+        for key in field_lengths.keys():
+            if (type(labelled_sample["data"][0][key]) is not int) and (
+                type(labelled_sample["data"][0][key]) is not float
+            ):
+                field_lengths[key] = len(labelled_sample["data"][0][key])
+            else:
+                field_lengths[key] = 1
+        print("\nNumber of entries per field:", field_lengths)
+
+    for i in range(3):
+        sample = os.listdir(recordings_folder_path)[i]
+        with open(os.path.join(recordings_folder_path, sample)) as sample_json:
+            labelled_sample = json.load(sample_json)
+        print("Number of Data Dictionaries:", len(labelled_sample["data"]))
 
 
 # Print result, do not change!
@@ -50,3 +89,16 @@ print(activities)
 # python [thisfilename.py] path/to/datafile.json
 
 # Output should be an integer corresponding to the step count you calculated
+
+
+def main():
+    labelled_activities_json_path = os.path.join(sys.argv[1])
+    class_records_folder = os.path.join(sys.argv[2])
+    with open(labelled_activities_json_path) as jsonfile:
+        labelled_activities = json.load(jsonfile)
+
+    data_analysis(class_records_folder)
+
+
+if __name__ == "__main__":
+    main()
